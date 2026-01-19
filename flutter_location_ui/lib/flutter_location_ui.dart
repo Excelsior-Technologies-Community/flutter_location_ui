8b3bd314de0cd6974ed97000753e4c1f4c472211
VajaVishal <vishalvaja2622004@gmail.com>
2026-01-19 16:35:50 +0530

    update code

--- flutter_location_ui/lib/flutter_location_ui.dart
+++ flutter_location_ui/lib/flutter_location_ui.dart
@@ -1,5 +1,9 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library flutter_location_ui;
+
+export 'screens/map_search_screen.dart';
+export 'services/osm_service.dart';
+export 'services/places_service.dart';
+export 'utils/constants.dart';
+export 'widgets/location_info_card.dart';
+export 'widgets/search_bar_widget.dart';
+export 'widgets/search_suggestion_list.dart';
